754a19c99234a9b84b737b3223d2a56f258704ef
Rafscps <160783539+Rafscps@users.noreply.github.com>
2024-03-04 02:53:11 -0300

    Create Form.php
    Html

---
+++ Form.php
@@ -0,0 +1,45 @@
+<section>
+
+<form>
+
+<h1>Inicio</h1>
+
+<div class="inputbox" >
+
+<ion-icon name="mail-outline"></ion-icon>
+
+<input type="email" required>
+
+<label for="">Usuario</label>
+
+</div>
+
+<div class="inputbox" >
+
+<ion-icon name="lock-closed-outline"></ion-icon>
+
+<input type="password" required>
+
+<label for="">Contraseña</label>
+
+</div>
+
+<div class="forget">
+
+<label for=""><input type="checkbox">Recordar</label>
+
+<a href="#">Olvidé la Contraseña</a>
+
+</div>
+
+<button>Iniciar</button>
+
+<div class="register">
+
+<p>No tengo una cuenta <a href="#">Registrate</a></p>
+
+</div>
+
+</form>
+
+</section>
